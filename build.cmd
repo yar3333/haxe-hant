b05f4570e88d1cf1249b5e90a17ad23d470cc301
Yaroslav <Yaroslav@YHOST.beeline>
2012-11-10 23:15:17 +0400

    * minor

--- build.cmd
+++ build.cmd
@@ -1,9 +1,11 @@
 @cd library
 
 del /Q ndll\Windows\*.ilk
+del /Q ndll\Windows\*.pdb
 del /Q src\neko\*.ncb
 del /Q src\neko\*.user
 del /Q src\neko\Debug\*
+del /Q src\neko\Release\*
 
 7z a -tzip ..\library.zip *
 
