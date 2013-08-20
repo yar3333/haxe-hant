a85b89b05a852fcc834ecff130f70f8e648505d5
Yaroslav <Yaroslav@localhost>
2013-08-20 15:28:46 +0400

    * rearrange files

--- build.cmd
+++ build.cmd
@@ -1,14 +1,3 @@
 @cd library
-
-del /Q ndll\Windows\*.ilk
-del /Q ndll\Windows\*.pdb
-del /Q src\neko\*.ncb
-del /Q src\neko\*.user
-del /Q src\neko\*.suo
-del /Q src\neko\*.o
-del /Q src\neko\Debug\*
-del /Q src\neko\Release\*
-
 7z a -tzip ..\library.zip *
-
 @cd ..
