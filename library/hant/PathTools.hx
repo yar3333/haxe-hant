1ab1200b3c8b21e83cb18e46703ab209eef6075b
Yaroslav <yar3333@gmail.com>
2014-01-06 02:28:22 +0400

    + PathTools.getRelative()

--- library/hant/PathTools.hx
+++ library/hant/PathTools.hx
@@ -1,8 +1,10 @@
 package hant;
 
+using StringTools;
+
 class PathTools 
 {
-	public static function path2normal(path:String) : String
+	public static function normalize(path:String) : String
 	{
 		path = StringTools.replace(path, "\\", "/");
 		
@@ -14,8 +16,43 @@ class PathTools
 		return path;
 	}
 	
-	public static function path2native(path:String) : String
+	public static function makeNative(path:String) : String
 	{
 		return Sys.systemName() != "Windows" ? path : StringTools.replace(path, "/", "\\");
 	}
+	
+	public static function getRelative(basePath:String, absolutePath:String) : String
+	{
+        if (basePath == null || basePath == "") basePath = sys.FileSystem.fullPath(".");
+        
+		basePath = basePath.replace("\\", "/");
+        absolutePath = absolutePath.replace("\\", "/");
+		
+        var r = "";
+        var commonPart = "";
+        var basePathFolders = basePath.split("/");
+        var absolutePathFolders = absolutePath.split("/");
+        var i = 0;
+        while (i < basePathFolders.length && i < absolutePathFolders.length)
+        {
+            if (basePathFolders[i].toLowerCase() == absolutePathFolders[i].toLowerCase())
+            {
+                commonPart += basePathFolders[i] + "/";
+            }
+            else
+            {
+                break;
+            }
+            i++;
+        }
+        if (commonPart.length > 0)
+        {
+            for (dir in basePath.substr(commonPart.length - 1).split("/"))
+            {
+                if (dir != "") r += "../";
+            }
+        }
+        r += absolutePath.substr(commonPart.length);
+        return r;
+    }	
 }
